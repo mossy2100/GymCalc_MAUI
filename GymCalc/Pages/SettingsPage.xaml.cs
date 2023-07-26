c10b572d7b0ea957bb5e13abc628a0b94914960d
Shaun Moss <shaun@astromultimedia.com>
2023-07-27 09:53:36 +1000

    Commented out colour scheme-related code.

--- GymCalc/Pages/SettingsPage.xaml.cs
+++ GymCalc/Pages/SettingsPage.xaml.cs
@@ -11,7 +11,7 @@ public partial class SettingsPage : ContentPage
         InitializeComponent();
 
         UnitsRadio.SelectedItem = Units.GetPreferred();
-        ColorSchemeRadio.SelectedItem = Preferences.Default.Get("ColorScheme", Theme.Orange);
+        // ColorSchemeRadio.SelectedItem = Preferences.Default.Get("ColorScheme", Theme.Orange);
     }
 
     private void OnUnitsSelectedItemChanged(object sender, EventArgs e)
@@ -20,39 +20,39 @@ public partial class SettingsPage : ContentPage
         Preferences.Default.Set("Units", (string)rbg.SelectedItem);
     }
 
-    private void OnColorSchemeSelectedItemChanged(object sender, EventArgs e)
-    {
-        // Get the theme they selected.
-        var rbg = (RadioButtonGroupView)sender;
-        var theme = (string)rbg.SelectedItem;
-
-        // Record their preference.
-        Preferences.Default.Set("ColorScheme", theme);
-
-        // Update the theme.
-        ICollection<ResourceDictionary> mergedDictionaries = Application.Current!.Resources.MergedDictionaries;
-        if (mergedDictionaries != null)
-        {
-            foreach (var resourceDictionary in mergedDictionaries)
-            {
-                if (resourceDictionary is PinkTheme or OrangeTheme || resourceDictionary.Source.ToString().Contains("Theme.xaml"))
-                {
-                    mergedDictionaries.Remove(resourceDictionary);
-                    break;
-                }
-            }
-
-            switch (theme)
-            {
-                case Theme.Pink:
-                    mergedDictionaries.Add(new PinkTheme());
-                    break;
-
-                case Theme.Orange:
-                default:
-                    mergedDictionaries.Add(new OrangeTheme());
-                    break;
-            }
-        }
-    }
+    // private void OnColorSchemeSelectedItemChanged(object sender, EventArgs e)
+    // {
+    //     // Get the theme they selected.
+    //     var rbg = (RadioButtonGroupView)sender;
+    //     var theme = (string)rbg.SelectedItem;
+    //
+    //     // Record their preference.
+    //     Preferences.Default.Set("ColorScheme", theme);
+    //
+    //     // Update the theme.
+    //     ICollection<ResourceDictionary> mergedDictionaries = Application.Current!.Resources.MergedDictionaries;
+    //     if (mergedDictionaries != null)
+    //     {
+    //         foreach (var resourceDictionary in mergedDictionaries)
+    //         {
+    //             if (resourceDictionary is PinkTheme or OrangeTheme || resourceDictionary.Source.ToString().Contains("Theme.xaml"))
+    //             {
+    //                 mergedDictionaries.Remove(resourceDictionary);
+    //                 break;
+    //             }
+    //         }
+    //
+    //         switch (theme)
+    //         {
+    //             case Theme.Pink:
+    //                 mergedDictionaries.Add(new PinkTheme());
+    //                 break;
+    //
+    //             case Theme.Orange:
+    //             default:
+    //                 mergedDictionaries.Add(new OrangeTheme());
+    //                 break;
+    //         }
+    //     }
+    // }
 }
