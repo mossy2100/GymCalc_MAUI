f5aa1ec6597cc810cff427f5df2d87598af04825
Shaun Moss <shaun@astromultimedia.com>
2023-11-30 11:34:47 +1000

    Changed frames around entry controls to borders.

--- GymCalc/MauiProgram.cs
+++ GymCalc/MauiProgram.cs
@@ -21,7 +21,10 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             })
-            .ConfigureMauiHandlers(handlers => { handlers.AddInputKitHandlers(); });
+            .ConfigureMauiHandlers(handlers =>
+            {
+                handlers.AddInputKitHandlers();
+            });
 
         builder.Services.AddMauiBlazorWebView();
 
